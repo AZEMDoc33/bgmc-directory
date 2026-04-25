a3f3603415025bd3408860a7a7ab94e1ac016add
Eric C <eric.cummins33@gmail.com>
2026-04-25 08:05:34 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'bgmc-directory-v1';
-const FILES_TO_CACHE = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+const FILES_TO_CACHE = ['./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(CACHE_NAME).then(function(c) { return c.addAll(FILES_TO_CACHE); }));
@@ -8,7 +8,7 @@ self.addEventListener('install', function(e) {
 
 self.addEventListener('activate', function(e) {
   e.waitUntil(caches.keys().then(function(keys) {
-    return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); }));
+    return Promise.all(keys.filter(function(k){return k!==CACHE_NAME;}).map(function(k){return caches.delete(k);}));
   }));
   self.clients.claim();
 });
@@ -17,11 +17,11 @@ self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(cached) {
       return cached || fetch(e.request).then(function(r) {
-        if (r && r.status === 200 && e.request.method === 'GET') {
-          caches.open(CACHE_NAME).then(function(c) { c.put(e.request, r.clone()); });
+        if (r && r.status===200 && e.request.method==='GET') {
+          caches.open(CACHE_NAME).then(function(c){c.put(e.request,r.clone());});
         }
         return r;
       });
-    }).catch(function() { return caches.match('./index.html'); })
+    }).catch(function(){ return caches.match('./index.html'); })
   );
 });
